b68c794cacc07abb49b92061da0cc054af4daa75
SamuelSubiraPrado <149213761+SamuelSubiraPrado@users.noreply.github.com>
2023-12-05 11:05:47 -0300

    Add files via upload

---
+++ aula40_calculadora.py
@@ -0,0 +1,51 @@
+#Calculadora 
+#solicitar n1, um operador e n2;
+
+continuar = True
+while continuar == True:
+
+    try:
+        n1= float(input("Digite o primeiro número: "))
+        print (n1)
+        operador = input("Digite o operador (+ ,-,/,*): ")
+        n2= float(input("Digite o segundo número: "))
+        print (n2)
+        resultado = 0
+
+        if operador == '+': 
+            resultado = n1 + n2
+            print (f"Seu resultado é {resultado:.2f}.")
+        elif operador == '-': 
+            resultado = n1 - n2
+            print (f"Seu resultado é {resultado:.2f}.")
+        elif operador == '*': 
+            resultado = n1 * n2
+            print (f"Seu resultado é {resultado:.2f}.")
+        elif operador == '/': 
+            resultado = n1 + n2
+            print (f"Seu resultado é {resultado:.2f}.")
+        else:
+            print("Você não digitou um operador valido.")
+
+        continuar=input ('Deseja continuar? (S/N): ')
+
+        if continuar == 's' or continuar == 'S':
+            continuar = True
+
+        else:
+            print ("Finalizado")
+            continuar = False
+        
+    except:
+        
+        print("Você digitou algo incorretamente, tenha certeza de digitar apenas números e operadores.")
+
+        continuar=input ('Deseja continuar? (S/N): ')
+
+        if continuar == 's' or continuar == 'S':
+            continuar = True
+
+        else:
+            print ("Finalizado")
+            continuar = False
+    
